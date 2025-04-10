8f3d995b5e8e94aac1285a97bd90f133f3c76219
Muthanandham <muthanandham.g@hash8.io>
2025-04-10 10:28:39 +0000

    first level

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ClipboardModule, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'my-angular';
+  title = 'Marvel';
+  valueToCopy: any;
+  template: string = '';
+  firstName: string = '';
+  lastName: string = '';
+  age: string = '';
+  showText: boolean = false;
+  constructor(private clipboard: Clipboard) {}
+  ngOninit() {}
+  onSubmit() {
+    if (this.firstName !== '' && this.lastName !== '' && this.age !== '') {
+      this.showText = true;
+
+      this.template = `Your Name is ${this.firstName} ${this.lastName} and you are ${this.age} Old.`;
+      this.clipboard.copy(this.template);
+    }
+  }
 }
