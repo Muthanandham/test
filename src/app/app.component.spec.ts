1fa30efed3c7fbf8d3595f070a1af428ed23c899
Muthanandham <muthanandham.g@hash8.io>
2025-04-10 11:27:57 +0000

    test cases fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-angular' title`, () => {
+  it(`should have the 'Marvel' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-angular');
+    expect(app.title).toEqual('Marvel');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Marvel Selection'
+    );
   });
 });
